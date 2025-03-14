0de29f5c5967778daccb47771713ebbc68c03748
Celic Rm <celicrmrr@gmail.com>
2025-03-14 15:34:48 -0600

    Cree la función de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosSecretos = [];
+
+function agregarAmigo() 
+{
+    const input = document.getElementById('amigo'); //Capturar el valor de campo de entrada
+    if (input.length == 0) //Validando que el campo no esté vacío
+        {
+        alert('Introduce el nombre de tu amigo');
+        return;
+        }
+    amigosSecretos.push(input); //Agregar la entrada del usuario a la lista de amigos
+    document.getElementById('amigo').value = ''; //Limpiando el campo de entrada
+}
+
